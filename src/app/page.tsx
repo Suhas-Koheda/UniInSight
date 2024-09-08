ea5b8b4670b811a5816e647c22b5f3438e161f08
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-09-09 03:00:58 +0530

    role based routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,18 @@ export default function Home() {
             <div className="flex flex-col items-center w-auto">
                 <div className="grid cols-1 md:flex md:justify-around ">
                     <div className="p-14 text-center md:p-24 bg-button-bg rounded-3xl md:m-4 m-4 shadow-inner">
-                        <h4><a href="/Dean">Dean</a></h4>
+                        <h4><a href="/Dean/login">Dean</a></h4>
                     </div>
                     <div className="p-14 text-center md:p-24 bg-button-bg rounded-3xl md:m-4 m-4 shadow-inner">
-                        <h4><a href="/Hod">HOD</a></h4>
+                        <h4><a href="/Hod/login">HOD</a></h4>
                     </div>
                 </div>
                 <div className="grid cols-1 md:flex md:justify-around ">
                     <div className="p-14 text-center md:p-24 bg-button-bg rounded-3xl md:m-4 m-4 shadow-inner">
-                        <h4><a href="/Faculty">Faculty</a></h4>
+                        <h4><a href="/Faculty/login">Faculty</a></h4>
                     </div>
                     <div className="p-14 text-center md:p-24 bg-button-bg rounded-3xl md:m-4 m-4 shadow-inner">
-                        <h4><a href="/Student">Student</a></h4>
+                        <h4><a href="/Student/login">Student</a></h4>
                     </div>
                 </div>
             </div>
